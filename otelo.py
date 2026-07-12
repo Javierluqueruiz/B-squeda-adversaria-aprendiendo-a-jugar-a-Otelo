18e3c6016288a279115256275489c3d1f96b19fc
Javier Luque <javluqrui@alum.us.es>
2026-07-12 19:50:41 +0200

    doc: revision de la sección 'Motor del Juego'

--- otelo.py
+++ otelo.py
@@ -6,8 +6,10 @@ import numpy as np
 
 class Otelo:
     def __init__(self):
+        #Inicialización del tablero de Otelo como una matriz de 8x8 con ceros
         self.tablero = np.zeros((8, 8), dtype=int)
 
+        #Posición inicial de las fichas en el tablero
         self.tablero[3][3] = 1
         self.tablero[3][4] = 2
         self.tablero[4][3] = 2
@@ -34,19 +36,23 @@ class Otelo:
     DIRECCIONES = [(0, 1), (1, 1), (1, 0), (1, -1), (0, -1), (-1, -1), (-1, 0), (-1, 1)]
     
     def es_movimiento_valido(self, fila, columna, jugador):
+        # Primero comprueba que la casilla esté vacía
         if self.tablero[fila, columna] != 0:
             return False;
         
         oponente = 1 if jugador == 2 else 2
 
+        # Recorre todas las direcciones posibles
         for df, dc in self.DIRECCIONES:
             f, c = fila + df, columna + dc
             fichas_oponentes_intermedias = 0
-
+            
+            # Continúa en la dirección mientras haya fichas del oponente 
             while  0<= f <8 and 0 <= c < 8 and self.tablero[f, c] == oponente:
                 fichas_oponentes_intermedias += 1
                 f, c = f + df, c + dc
 
+            # Comprueba si la dirección termina con una ficha del jugador actual y si hay fichas del oponente en medio
             if 0 <= f < 8 and 0 <= c < 8:
                 if self.tablero[f, c] == jugador and fichas_oponentes_intermedias > 0:
                     return True
@@ -55,13 +61,16 @@ class Otelo:
     
     def ejecutar_movimiento(self, fila, columna, jugador):
 
+        # Utiliza el método anterior 
         if not self.es_movimiento_valido(fila, columna, jugador): 
             return False
-    
+
+        # Coloca la ficha del jugador
         self.tablero[fila, columna] = jugador
 
         oponente = 1 if jugador == 2 else 2
 
+        # Recorre las direcciones para cambiar las fichas del oponente
         for df, dc, in self.DIRECCIONES:
             f, c = fila + df, columna + dc
             fichas_oponente = []
@@ -78,7 +87,7 @@ class Otelo:
 
         return True
     
-
+    # Recorre el tablero y devuelve una lista de movimientos válidos para el jugador
     def obtener_movimientos_validos(self, jugador):
         movimientos = []
         for f in range(8):
@@ -92,9 +101,10 @@ class Otelo:
         movimientos_blancas = self.obtener_movimientos_validos(1)
         movimientos_negras = self.obtener_movimientos_validos(2)
 
+        # Si ningún jugador tiene movimientos válidos
         if len(movimientos_blancas) == 0 and len(movimientos_negras) == 0:
             return True
-
+        # Si el tablero está lleno
         if np.count_nonzero(self.tablero) == 64:
             return True
         
